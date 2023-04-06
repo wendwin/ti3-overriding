5dfd069a09a8ed0e303f605a4cfa74ab2ad5e925
wendwin <wendwin12@gmail.com>
2023-04-06 11:14:57 +0700

    membuat class parent

---
+++ overriding.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self, name, email, depart, comm) -> None:
+        self.name = name
+        self.email = email
+        self.depart = depart
+        self.comm = comm
+    
+    def showEmployee(self):
+        print("Name : {}\nemail : {}\nDepartment : {} \nCommision : $ {} ".format(self.name, self.email, self.department, self.commission))
